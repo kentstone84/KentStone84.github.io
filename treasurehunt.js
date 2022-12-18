378878f69969d97821a13c0619a563ef2eb98cb4
Kent Stone <69402437+kentstone84@users.noreply.github.com>
2022-12-18 01:14:42 -0500

    Update treasurehunt.js

--- treasurehunt.js
+++ treasurehunt.js
@@ -5,21 +5,21 @@
 // takes an id of an apple on the HTML page from an on click
 // and changes the HTML element that the ID represents to an emoji
 // var zombie = Math.floor(Math.random() * 8 )
- var zombie = Math.floor(Math.random()*8)
- var diamond = Math.floor(Math.random()*8)
- if (diamond === zombie) {
-  var diamond = Math.floor(Math.random()*8)
- }
- // console.log(zombie, diamond);
- const imgChanger = (id) => {
-  if (diamond === id){
-    document.getElementById(id).innerHTML = "💎";
-    alert("Yippee Ki Yay! You Won!")
-  } else if(zombie === id){
-        alert("U R CuRsEd!.!.!")
-    return document.getElementById(id).innerHTML = "🧟";
+const imgChanger = (id) => {
+  // Generate new random numbers for zombie and diamond if they are equal
+  if (diamond === zombie) {
+    zombie = Math.floor(Math.random() * 8);
+    diamond = Math.floor(Math.random() * 8);
   }
-    else {
-    return document.getElementById(id).innerHTML = "⌛";
-    }
-}
+
+  // Change HTML element based on id
+  if (diamond === id) {
+    document.getElementById(id).innerHTML = "💎";
+    alert("Congratulations! You won!");
+  } else if (zombie === id) {
+    document.getElementById(id).innerHTML = "🧟‍♂️";
+    alert("Oh no! You lost!");
+  } else {
+    document.getElementById(id).innerHTML = "⌛";
+  }
+};
